50a21edd4acc46ffdf939cadbe927eab1dc64e73
hw <2195792949@qq.com>
2020-01-14 15:21:47 +0800

    测试wei

---
+++ test_12_08/text_12_08/text.c
@@ -0,0 +1,28 @@
+
+//int get_max(int x,int y)
+//{
+//	int z;
+//	if (x>y)
+//		z= x;
+//	else
+//		z = y;
+//	return z;
+//}
+//
+//int main()
+//{
+//	int num1 = 10;
+//	int num2 = 20;
+//	//要求：完成get_max函数
+//	int max = get_max(num1, num2);
+//	printf("max = %d\n", max);
+//	return 0;
+//}
+//}
+#include <stdio.h>
+int main()
+{
+	 int arr[10] = { 0 };
+	//char ch[10] = "hello bit" ;
+	printf("%d\n", arr[2]);
+}
